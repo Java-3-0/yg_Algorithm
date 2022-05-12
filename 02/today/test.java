ce8938524d9b6b4fd5f64ffd92134c1449aa0b88
younggilPark <ygpark1230@gmail.com>
2022-05-12 22:16:37 +0900

    0512

--- 02/today/test.java
+++ 02/today/test.java
@@ -6,6 +6,7 @@ import java.io.*;
 public class Boj_16724_피리부는사나이 {
     static int N, M;
     static int[][] arr;
+    static int cnt = 1;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -19,17 +20,16 @@ public class Boj_16724_피리부는사나이 {
             }
         }
 
-        int cnt = 1;
         // 번호 매기기
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                run(i, j, cnt);
+                run(i, j);
             }
         }
 
         System.out.println(--cnt);
     }
-    static void run(int i, int j, int cnt) {
+    static void run(int i, int j) {
         if(arr[i][j] != 'U' && arr[i][j] != 'D' && arr[i][j] != 'L' && arr[i][j] != 'R')
             return;
 
@@ -45,18 +45,9 @@ public class Boj_16724_피리부는사나이 {
 
             // 다음 위치 범위 내 여부 판단
             if(nx < 0 || nx >= N || ny < 0 || ny >= M || arr[nx][ny] == cnt) {
-                System.out.println("!!!!!!!!!");
-                for (int[] line: arr ) {
-                    for (int k = 0; k < line.length; k++) {
-                        System.out.print(line[k]+ " ");
-                    }
-                    System.out.println();
-                }
                 cnt++;
                 break;
             }
-
-            // 다음 위치 숫자로 변경하기
             x = nx;
             y = ny;
         }
